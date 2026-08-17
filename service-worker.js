e693208cb42e07a7e25c662c65ba5063b57448fd
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-08-17 14:28:26 -0300

    Atualiza cache para incluir novo ícone do app

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'avaliacao-pratica-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './evaluation-data.mjs', './scoring.mjs', './manifest.webmanifest'];
+const CACHE = 'avaliacao-pratica-v2';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './evaluation-data.mjs', './scoring.mjs', './manifest.webmanifest', './app-icon-192.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
